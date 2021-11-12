c1b281a322d0ebcfb70a27ea8b2d845bbe9461b4
khali <delaureaka@gmail.com>
2021-11-12 21:28:15 +0100

    adding slider

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ if(!event.target.matches(".dropLine")){
 		}
 	}
 }}
+
+// slider script
+var slideIndex = 1;
+slider(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+ slider(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+ slider(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+ }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+  }
+ slides[slideIndex-1].style.display = "block";
+ dots[slideIndex-1].className += " active";
+}
